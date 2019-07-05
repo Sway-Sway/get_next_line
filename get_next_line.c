628f9d5438ae34dbc1e872fd3b5a85c5e795a72f
Joshua KWAYIBA <jkwayiba@c4r21s4.wethinkcode.co.za>
2019-07-05 17:19:39 +0200

    finally finshed gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,47 @@
 /*   By: jkwayiba <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 22:45:12 by jkwayiba          #+#    #+#             */
-/*   Updated: 2019/07/04 22:46:39 by jkwayiba         ###   ########.fr       */
+/*   Updated: 2019/07/05 17:17:21 by jkwayiba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-
-int		read_chunk(int fd, char **buffer)
+char	*read_chunk(int fd, char **buffer)
 {
 	char	*placeholder;
 	char	temporary[BUFF_SIZE + 1];
 	ssize_t	size;
+	int		ret;
+	char	*ptr;
 
-	size = read(fd, temporary, BUFF_SIZE);
-	if (size < 1)
-		return (0);
-	temporary[size] = '\0';
-	placeholder = ft_strjoin(*buffer, temporary);
-	ft_strdel(buffer);
-	*buffer = ft_strdup(placeholder);
-	ft_strdel(&placeholder);
-	return (1);
+	while ((ptr = ft_strchr(*buffer, '\n')) == NULL)
+	{
+		size = read(fd, temporary, BUFF_SIZE);
+		if (size < 1)
+			return (NULL);
+		temporary[size] = '\0';
+		placeholder = ft_strjoin(*buffer, temporary);
+		ft_strdel(buffer);
+		*buffer = ft_strdup(placeholder);
+		ft_strdel(&placeholder);
+	}
+	return (ptr);
+}
+
+char	*ft_strdupdel(char **str)
+{
+	char	*tmp;
+
+	tmp = ft_strdup(*str);
+	ft_strdel(str);
+	return (tmp);
 }
 
 int		get_next_line(int fd, char **line)
 {
 	static char *str[255];
 	char		buff[BUFF_SIZE + 1];
-	int			ret;
 	char		*ptr;
 	char		*tmp;
 
@@ -42,26 +54,16 @@ int		get_next_line(int fd, char **line)
 		return (-1);
 	if (str[fd] == NULL)
 		str[fd] = ft_strnew(0);
-	while ((ptr = ft_strchr(str[fd],'\n')) == NULL)
-	{
-		ret = read_chunk(fd, &(str[fd]));
-		if (ret == 0)
-			break ;
-	}
+	ptr = read_chunk(fd, &(str[fd]));
 	if (ptr != NULL)
 	{
 		*ptr = '\0';
-		*line = ft_strdup(str[fd]);
 		tmp = ft_strdup(ptr + 1);
-		ft_strdel(&(str[fd]));
-		str[fd] = ft_strdup(tmp);
-		ft_strdel(&tmp);
+		*line = ft_strdupdel(&str[fd]);
+		str[fd] = ft_strdupdel(&tmp);
 	}
 	else if (ft_strlen(str[fd]) != 0)
-	{
-		*line = ft_strdup(str[fd]);
-		ft_strdel(&(str[fd]));
-	}
+		*line = ft_strdupdel(line);
 	else
 		return (0);
 	return (1);
